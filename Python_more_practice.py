0b16e926ddc6a91d73ad54c427c46bf27381a0c9
Michael Hertel <mhertel278@gmail.com>
2021-01-03 16:26:52 -0600

    through module 3.4

--- Python_more_practice.py
+++ Python_more_practice.py
@@ -1,7 +1,16 @@
-temperature = int(input("What is the temperature outside? "))
+#temperature = int(input("What is the temperature outside? "))
 
-if temperature > 80:
-    print("Turn on the AC.")
+#if temperature > 80:
+ #   print("Turn on the AC.")
 
-else:
-    print("Open the windows.")
+#else:
+ #   print("Open the windows.")
+# 1 Import datetime module
+#import datetime
+# Use the now() attribute on the datetime class to get the present time.
+#now = datetime.datetime.now()
+#print the current date and time
+#print(now)
+import datetime as dt
+now = dt.datetime.now()
+print(now)
