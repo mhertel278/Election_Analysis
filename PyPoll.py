93264fe5f0862c6bffcfac553840a89fe4706c1f
Michael Hertel <mhertel278@gmail.com>
2021-01-06 22:17:51 -0600

    added candidate list and dictionary

--- PyPoll.py
+++ PyPoll.py
@@ -18,14 +18,45 @@ file_to_save = os.path.join('analysis', 'election_analysis.txt')
  # Initialize the total vote counter as a variable at 0
 total_votes = 0
 
+# create a list to store Candidate names
+candidate_options = []
+
+# create dictionary to store candidates with their vote totals
+candidate_votes = {}
 #Open the election results and read the file
 with open(file_to_load) as election_data:
 
     #To do read and analyze the data here
     file_reader = csv.reader(election_data)
 
-    # Print the header row
+    # Read the header row
     headers = next(file_reader)
-    print(headers)
+    
+    
+    # loop over data and increment the total votes
+    for row in file_reader:
+        # add 1 to the total votes
+        total_votes += 1
 
-    # Print each row in the CSV file
+        #create variable to store the candidate name in the given row
+        candidate_name = row[2]
+
+        # if statment to test if the candidate in the current row is not already in the list of candidates
+        if candidate_name not in candidate_options:
+            # if name not in the list, add it to the list
+            candidate_options.append(candidate_name)
+            
+            # add the candidate name to the candidate votes dict as a key with 0 votes
+            candidate_votes[candidate_name] = 0
+        
+        # add 1 to the candidate votes in the dict
+        candidate_votes[candidate_name] += 1
+    
+    for candidate_name in candidate_options:
+
+        votes = candidate_votes[candidate_name]
+
+       
+        
+# print the total votes
+print(candidate_votes)
