b73fe52b79434c68540393de65edd21e7a2424fa
Michael Hertel <mhertel278@gmail.com>
2021-01-08 12:21:54 -0600

    remove practice files from folder

--- Python_Fstring_practice.py
+++
@@ -1,4 +0,0 @@
-my_votes = int(input("How many votes did you get in the election? "))
-total_votes = int(input("What is the total votes in the election? "))
-percentage_votes = (my_votes / total_votes) * 100
-print(f"I received {percentage_votes}% of the total votes.")
